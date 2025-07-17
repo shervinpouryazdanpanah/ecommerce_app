d511cc69da872ca709b50fd5482c9696f132cc52
shervinpouryazdanpanah <shervin.yazdan75@gmail.com>
2025-07-17 16:17:04 +0330

    create login ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:ecommerce/app/routes/routes.dart';
+import 'package:ecommerce/login.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/get_route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final routes = [GetPage(name: '/login', page: () => Login())];
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      getPages: AppRoutes.routes,
+      initialRoute: '/login',
+      getPages: routes,
     );
   }
 }
