2c13b809757896d95d6374d395aaa879d2fd5b8d
shervinpouryazdanpanah <shervin.yazdan75@gmail.com>
2025-07-22 23:36:03 +0330

    worked on categories page

--- lib/categories.dart
+++ lib/categories.dart
@@ -2,116 +2,35 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Categories extends StatelessWidget {
+  const Categories({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xAAE9F0FF),
-      body: Center(
-        child: Container(
-          width: 500,
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(16)),
-          margin: EdgeInsets.only(top: 50, bottom: 50),
-          padding: EdgeInsets.all(40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Logo",
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.black),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Categories",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w700),
+            ),
+            Expanded(
+              child: Divider(
+                color: Colors.grey,
+                thickness: 1.0,
               ),
-              SizedBox(
-                height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(width: 10.0, color: Colors.black),
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  suffixIcon: Icon(CupertinoIcons.profile_circled),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    suffixIcon: Icon(CupertinoIcons.lock),
-                    border: OutlineInputBorder(),
-                    labelStyle: TextStyle(color: Colors.black87)),
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: GestureDetector(
-                  onTap: () {
-                    print('Text clicked!');
-                  },
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                    padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(vertical: 20)),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account?",
-                    style: TextStyle(color: Colors.black87),
-                  ),
-                  MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () {
-                        print('Text clicked!');
-                      },
-                      child: Text(
-                        'Users',
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
